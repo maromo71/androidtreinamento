a1bd7a6c4887cc92df7c2fba71c24b8368f777fb
MARCOS ROBERTO DE MORAES <marcos.moraes20@etec.sp.gov.br>
2021-12-25 10:41:58 -0300

    Commit ListaContatos

--- ListaDeContatos/app/src/main/java/br/edu/faculdade/listadecontatos/MainActivity.java
+++ ListaDeContatos/app/src/main/java/br/edu/faculdade/listadecontatos/MainActivity.java
@@ -1,14 +1,29 @@
 package br.edu.faculdade.listadecontatos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listaContatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaContatos = findViewById(R.id.listaContatos);
+
+        listaContatos.setOnItemClickListener((adapterView, view, i, l) -> {
+            String nome = ((TextView) view).getText().toString();
+            AlertDialog.Builder cx = new AlertDialog.Builder(MainActivity.this);
+            cx.setTitle("Contato Selecionado");
+            cx.setMessage("Contato: " + nome);
+            cx.setNeutralButton("OK", null);
+            cx.show();
+        });
     }
 }
